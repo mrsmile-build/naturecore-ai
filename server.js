b4a720cd52c203e17265b2a215105ff3876a00a0
mrsmile-build <mrsmile4569@gmail.com>
2026-05-27 18:04:14 +0100

    Nature frontend

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = 4000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Frontend running on port ${PORT}`);
+});
