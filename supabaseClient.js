5a8c1cf4e7d96ba4f75900f1c54beabf95145ac9
mrsmile-build <mrsmile4569@gmail.com>
2026-05-29 10:42:34 +0100

    Fix frontend API connection

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,12 +1,7 @@
-‎const { createClient } = require('@supabase/supabase-js');
-‎
-‎const supabaseUrl = 'https://kwuutumzvdcsmbioqhbh.supabase.co';
-‎const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3dXV0dW16dmRjc21iaW9xaGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3NTAzOTEsImV4cCI6MjA5NTMyNjM5MX0.7_Eatvd75ez4kGv745Fd7bF6pyKMp1LYl-hNPvierbA';
-‎
-‎const supabase = createClient(
-‎  supabaseUrl,
-‎  supabaseKey
-‎);
-‎
-‎module.exports = supabase;
-‎EOF
+const { createClient } = require('@supabase/supabase-js');
+const supabaseUrl = 'https://kwuutumzvdcsmbioqhbh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3dXV0dW16dmRjc21iaW9xaGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3NTAzOTEsImV4cCI6MjA5NTMyNjM5MX0.7_Eatvd75ez4kGv745Fd7bF6pyKMp1LYl-hNPvierbA';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
