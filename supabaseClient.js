7395caee989753b71feb96b3d29f478c54c48473
mrsmile-build <mrsmile4569@gmail.com>
2026-05-29 10:14:52 +0100

    Fix Supabase client

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,6 @@
 ‎const { createClient } = require('@supabase/supabase-js');
 ‎
 ‎const supabaseUrl = 'https://kwuutumzvdcsmbioqhbh.supabase.co';
-‎
 ‎const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3dXV0dW16dmRjc21iaW9xaGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3NTAzOTEsImV4cCI6MjA5NTMyNjM5MX0.7_Eatvd75ez4kGv745Fd7bF6pyKMp1LYl-hNPvierbA';
 ‎
 ‎const supabase = createClient(
@@ -11,4 +10,3 @@
 ‎
 ‎module.exports = supabase;
 ‎EOF
-
